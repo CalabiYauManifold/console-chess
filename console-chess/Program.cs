e1e584cbae1f07d9fd16f0544ec378985d5e7e6f
CalabiYauManifold <170461818+CalabiYauManifold@users.noreply.github.com>
2024-07-09 08:55:31 -0300

    Created Position class

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace console_chess
+﻿using board;
+
+namespace console_chess
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Position P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
         }
     }
 }
